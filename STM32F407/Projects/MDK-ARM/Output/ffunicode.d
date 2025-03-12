b6e2d1c3207c08095d3dc3005fea5dadeb7ae121
玄黄 <72674992+soda-code@users.noreply.github.com>
2025-03-12 23:01:14 +0800

    Delete STM32F407/Projects/MDK-ARM/Output directory

--- STM32F407/Projects/MDK-ARM/Output/ffunicode.d
+++
@@ -1,4 +0,0 @@
-.\output\ffunicode.o: ..\..\Middlewares\FATFS\source\ffunicode.c
-.\output\ffunicode.o: ..\..\Middlewares\FATFS\source\ff.h
-.\output\ffunicode.o: ..\..\Middlewares\FATFS\source\ffconf.h
-.\output\ffunicode.o: D:\KEIL\ARM\ARMCC\Bin\..\include\stdint.h
--- STM32F407/Projects/MDK-ARM/Output/qrcodegen.d
+++
@@ -1,9 +0,0 @@
-.\output\qrcodegen.o: ..\..\Middlewares\LVGL\GUI\lvgl\src\extra\libs\qrcode\qrcodegen.c
-.\output\qrcodegen.o: D:\KEIL\ARM\ARMCC\Bin\..\include\assert.h
-.\output\qrcodegen.o: D:\KEIL\ARM\ARMCC\Bin\..\include\limits.h
-.\output\qrcodegen.o: D:\KEIL\ARM\ARMCC\Bin\..\include\stdlib.h
-.\output\qrcodegen.o: D:\KEIL\ARM\ARMCC\Bin\..\include\string.h
-.\output\qrcodegen.o: ..\..\Middlewares\LVGL\GUI\lvgl\src\extra\libs\qrcode\qrcodegen.h
-.\output\qrcodegen.o: D:\KEIL\ARM\ARMCC\Bin\..\include\stdbool.h
-.\output\qrcodegen.o: D:\KEIL\ARM\ARMCC\Bin\..\include\stddef.h
-.\output\qrcodegen.o: D:\KEIL\ARM\ARMCC\Bin\..\include\stdint.h
